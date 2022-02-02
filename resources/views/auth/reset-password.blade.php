1093a3e0e45e7dff10c7f77a3804e2d21c421663
elmasryn <suspecious002@hotmail.com>
2022-02-02 14:32:35 +0200

    frontend layout 3

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,46 +1,69 @@
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!-- 
+ developing by : Mohamed Elmasry
+ Github        : https://github.com/elmasryn
+ Email         : elmasry_n@hotmail.com 
+-->
+    <x-app-layout>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <div class="container">
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
-
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            <div class="wrap-breadcrumb">
+                <ul>
+                    <li class="item-link">
+                        <a href="/" class="link">home</a>
+                    </li>
+                    <li class="item-link">
+                        <span>Reset Password</span>
+                    </li>
+                </ul>
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
+                    <div class=" main-content-area">
+                        <div class="wrap-login-item ">
+                            <div class="register-form form-item ">
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+                                <!-- Validation Errors -->
+                                <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                                <form class="form-stl" name="frm-login" method="POST"
+                                    action="{{ route('password.update') }}">
+                                    @csrf
+
+                                    <!-- Password Reset Token -->
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                    <!-- Email Address -->
+                                    <fieldset class="wrap-input">
+                                        <label for="frm-reg-email">Email Address*</label>
+                                        <input type="email" id="frm-reg-email" name="email" placeholder="Email address"
+                                            value="{{ old('email', $request->email) }}">
+                                    </fieldset>
+                                    <!-- Password -->
+                                    <fieldset class="wrap-input item-width-in-half left-item">
+                                        <label for="frm-reg-pass">Password*</label>
+                                        <input type="password" id="frm-reg-pass" name="password" placeholder="Password"
+                                            autocomplete="password">
+                                    </fieldset>
+                                    <!-- Confirm Password -->
+                                    <fieldset class="wrap-input item-width-in-half ">
+                                        <label for="frm-reg-cfpass">Confirm Password*</label>
+                                        <input type="password" id="frm-reg-cfpass" name="password_confirmation"
+                                            placeholder="Confirm Password" autocomplete="password_confirmation">
+                                    </fieldset>
+
+                                    <input type="submit" class="btn btn-sign" value="Reset Password" name="confirm">
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!--end main products area-->
+                </div>
             </div>
+            <!--end row-->
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+        </div>
+        <!--end container-->
+    </x-app-layout>
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,37 +1,55 @@
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!-- 
+ developing by : Mohamed Elmasry
+ Github        : https://github.com/elmasryn
+ Email         : elmasry_n@hotmail.com 
+-->
+<x-app-layout>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+    <div class="container">
+
+        <div class="wrap-breadcrumb">
+            <ul>
+                <li class="item-link">
+                    <a href="/" class="link">home</a>
+                </li>
+                <li class="item-link">
+                    <span>Verification Email</span>
+                </li>
+            </ul>
         </div>
 
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-            </div>
-        @endif
+        <div class="row">
+            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
+                <div class=" main-content-area">
+                    <div class="wrap-login-item ">
+                        <div class="register-form form-item ">
 
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
+                            @if (session('status') == 'verification-link-sent')
+                                <div class="mb-4 font-medium text-sm text-green-600">
+                                    {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                                </div>
+                            @endif
 
-                <div>
-                    <x-button>
-                        {{ __('Resend Verification Email') }}
-                    </x-button>
+                            <form class="form-stl" name="frm-login" method="POST"
+                                action="{{ route('verification.send') }}">
+                                @csrf
+                                <input type="submit" class="btn btn-sign" value="Resend Verification Email"
+                                    name="Verification Email">
+                            </form>
+
+                            <form class="form-stl" name="frm-login" method="POST"
+                                action="{{ route('logout') }}">
+                                @csrf
+                                <input type="submit" class="btn btn-sign" value="Log Out" name="Log Out">
+                            </form>
+
+                        </div>
+                    </div>
                 </div>
-            </form>
-
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
-                </button>
-            </form>
+                <!--end main products area-->
+            </div>
         </div>
-    </x-auth-card>
+        <!--end row-->
+    </div>
+    <!--end container-->
+</x-app-layout>
